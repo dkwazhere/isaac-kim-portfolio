8e9ee0776d6fcf5d23ee2ecbc5f69a58185bf089
dkwazhere <dhealth77@gmail.com>
2018-06-18 21:55:05 -0700

    added aboutmecomponent

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { Button } from 'reactstrap';
 import Header from './components/Navbar';
+import Aboutme from './components/Aboutme';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Aboutme />
       </div>
     );
   }
