c15da567ac133a17b9492448ca5a1d10cc2b3021
randall ishino <rishino24@gmail.com>
2018-06-18 20:59:47 -0700

    navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,29 @@
 import React from "react";
 import './Navbar.css';
+import { Navbar, Nav,NavItem,MenuItem,NavDropdown} from "react-bootstrap";
+import image from '../../images/whitelogo.png';
+const Header = () => {
+  return (
+    <Navbar inverse>
+      <NavItem className="link" eventKey={1} href="#">
+        Home
+      </NavItem>
+      <NavItem className="link" eventKey={2} href="#">
+        About
+      </NavItem>
+      <NavItem eventKey={3} href="#">
+        <img className="logo-img" src={image}></img>
+      </NavItem>
 
+      <NavItem className="link" eventKey={4} href="#">
+        Gallery
+      </NavItem>
 
+      <NavItem className="link" eventKey={5} href="#">
+        Contact
+      </NavItem>
+    </Navbar>
+  );
+};
 
-export default Navbar;
+export default Header;
