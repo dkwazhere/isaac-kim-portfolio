6dca8a40bde52d334b004be27c2c5a5f535bca0c
randall ishino <rishino24@gmail.com>
2018-06-18 21:56:47 -0700

    topintro component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { Button } from 'reactstrap';
 import Header from './components/Navbar';
+import Topintro from './components/Topintro';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
+        <Topintro />
       </div>
     );
   }
