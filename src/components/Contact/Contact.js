f28c5e8e294b91003637d08b99159cf0ab3dbbcf
dkwazhere <dhealth77@gmail.com>
2018-06-25 20:37:31 -0700

    added some row and cols

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,36 +1,47 @@
 import React, { Component } from "react";
 import "./Contact.css";
 import contactpic from "../../images/_MG_2737.png";
+import { Col, Row, Container } from "reactstrap";
 
 
 class Contact extends Component {
     render() {
         return(
-            <div className='aboutme'>
-                <h1>
-                    Isaac Kim
-                </h1>
-                <h3 className='header-three'>
-                    Contact
-                </h3>
-                <div className='contact-pic'>
-                    <img src={contactpic}></img>
-                </div>
-                <div className='aboutme-text'>
-                    <h4>Let's get in touch.</h4>
-                    <br />
-                    <br />
-                    I am always looking for inspirational projects for which I can help find solutions or create beautiful imagery. Feel free to send me a message with some information about the project and I will get back to you as soon as I can.
-                    <br />
-                    <br />
-                    <h4>Email</h4>
-                    ikims1027@gmail.com
-                    <br />
-                    <br />
-                    <h4 className="contact-bottom">LinkedIn</h4>
-                    www.linkedin.com/in/designerisaackim
-                </div>
-            </div>
+            <Container className='aboutme'>
+                <Row>
+                    <h1>
+                        Isaac Kim
+                    </h1>
+                </Row>
+                <Row>
+                    <h3 className='header-three'>
+                        Contact
+                    </h3>
+                </Row>
+                <Row>
+                    <Col sm="6">
+                        <div className='contact-pic'>
+                            <img src={contactpic}></img>
+                        </div>
+                    </Col>
+                    <Col sm="6">
+                    <div className='aboutme-text'>
+                            <h4>Let's get in touch.</h4>
+                            <br />
+                            <br />
+                            I am always looking for inspirational projects for which I can help find solutions or create beautiful imagery. Feel free to send me a message with some information about the project and I will get back to you as soon as I can.
+                            <br />
+                            <br />
+                            <h4>Email</h4>
+                            ikims1027@gmail.com
+                            <br />
+                            <br />
+                            <h4 className="contact-bottom">LinkedIn</h4>
+                            www.linkedin.com/in/designerisaackim
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./Gallery.css";
+import { Col, Row, Container } from "reactstrap";
 
 const Gallery = () => {
     return(
-        <div>
-            <h2 className="academic-projects">Academic Projects</h2>
-            <div className="box">
-            <p>School Project Images Slideshow</p>
-            </div>
-            <h2 className="freelance-projects">Freelance Projects</h2>
-            <div className="box">
-            <p>Freelance Project Images Slideshow</p>
-            </div>
-            </div>
+        <Container className="gallery-container">
+            <Row>
+                <h2 className="academic-projects">Academic Projects</h2>
+            </Row>
+            <Row>
+                <div className="box">
+                    <p>School Project Images Slideshow</p>
+                </div>
+            </Row>
+            <Row>
+                <h2 className="freelance-projects">Freelance Projects</h2>
+            </Row>
+            <Row>
+                <div className="box">
+                    <p>Freelance Project Images Slideshow</p>
+                </div>
+            </Row>
+        </Container>
     )
 }
 
